d8d8958d5c66f444c6f823638f7c3b61f177ad73
Watson <watsonyan001@163.com>
2020-07-16 14:35:50 +0800

    init

--- src/com/hc/actions/LoginAction.java
+++ src/com/hc/actions/LoginAction.java
@@ -1,16 +1,29 @@
 package com.hc.actions;
 
 import java.io.Console;
+import java.util.Map;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class LoginAction extends ActionSupport {
+public class LoginAction extends ActionSupport{
+	public LoginAction() {
+		// TODO Auto-generated constructor stub
+		request = (Map)ActionContext.getContext().get("request");
+		
+	}
 	@Override
 	public String execute() {
+		request.put("name", "watson");
 		System.out.println(getUsername() + "  " + getPassword());
-		
+		if (username=="" || username == null) {
+			this.addFieldError("username", "name is error");
+			return "invalid";
+		}
 		return SUCCESS;
 	}
+	
+	private Map request;
 	
 	private String username;
 	private String password;
@@ -27,4 +40,5 @@ public class LoginAction extends ActionSupport {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
 }
